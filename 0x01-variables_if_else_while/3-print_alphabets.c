bf70b085421306ca2a42d592f52a2bd15f2f78d4
Debfevlo <deborahfevlo@gmail.com>
2023-02-21 06:41:09 +0000

    hellllo

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-char alp[52] = "bcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+	char ch;
 
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n')
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar (ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
 	return (0);
 }
