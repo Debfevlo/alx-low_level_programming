9e8e8c0b949b6dc29b866c4ef15d8086a44f88ac
Debfevlo <deborahfevlo@gmail.com>
2023-02-23 02:00:55 +0000

    hello

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_sign - prints the signof a member
+ * @c: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1)
+	}
+	else if (n == 0)
+}
